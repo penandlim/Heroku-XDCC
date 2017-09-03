99d8d32a7b711bc24ab087e755c05e7f78dd65f1
John Lim (Win Desktop) <limjohn@uw.edu>
2017-09-03 00:42:40 -0700

    mobile support

--- public/vip/main.js
+++ public/vip/main.js
@@ -1,48 +1,45 @@
 /**
  * Created by John LIm on 9/2/2017.
  */
-function createCORSRequest(method, url) {
-    var xhr = new XMLHttpRequest();
-    if ("withCredentials" in xhr) {
-
-        // Check if the XMLHttpRequest object has a "withCredentials" property.
-        // "withCredentials" only exists on XMLHTTPRequest2 objects.
-        xhr.open(method, url, true);
-
-    } else if (typeof XDomainRequest != "undefined") {
-
-        // Otherwise, check if XDomainRequest.
-        // XDomainRequest only exists in IE, and is IE's way of making CORS requests.
-        xhr = new XDomainRequest();
-        xhr.open(method, url);
-
-    } else {
-
-        // Otherwise, CORS is not supported by the browser.
-        xhr = null;
-
-    }
-    return xhr;
-}
-
-
-
-//
-//
-// var xhr = function() {
+// function createCORSRequest(method, url) {
 //     var xhr = new XMLHttpRequest();
-//     return function( method, url, callback ) {
-//         xhr.onreadystatechange = function() {
-//             if ( xhr.readyState == 4 ) {
-//                 if (xhr.status == 200) {
-//                     callback( xhr.responseXML );
-//                 }
-//             }
-//         };
-//         xhr.open( method, url );
-//         xhr.send();
-//     };
-// }();
+//     if ("withCredentials" in xhr) {
+//
+//         // Check if the XMLHttpRequest object has a "withCredentials" property.
+//         // "withCredentials" only exists on XMLHTTPRequest2 objects.
+//         xhr.open(method, url, true);
+//
+//     } else if (typeof XDomainRequest != "undefined") {
+//
+//         // Otherwise, check if XDomainRequest.
+//         // XDomainRequest only exists in IE, and is IE's way of making CORS requests.
+//         xhr = new XDomainRequest();
+//         xhr.open(method, url);
+//
+//     } else {
+//
+//         // Otherwise, CORS is not supported by the browser.
+//         xhr = null;
+//
+//     }
+//     return xhr;
+// }
+
+
+var xhr = function() {
+    var xhr = new XMLHttpRequest();
+    return function( method, url, callback ) {
+        xhr.onreadystatechange = function() {
+            if ( xhr.readyState == 4 ) {
+                if (xhr.status == 200) {
+                    callback( xhr.responseXML );
+                }
+            }
+        };
+        xhr.open( method, url );
+        xhr.send();
+    };
+}();
 
 var txt = "";
 
@@ -54,19 +51,30 @@ var stream = null;
 var paused = false;
 var shouldShuffle = false;
 
+var isMobile = false; //initiate as false
+// device detection
+if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(navigator.userAgent)
+    || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(navigator.userAgent.substr(0,4))) isMobile = true;
+
 function processData(data) {
     var tracks = data.getElementsByTagName("track");
-    console.log(tracks);
-    txt += "<ul>";
+    // console.log(tracks);
+
+    var mobileClassTxt = isMobile ? " class='mobile' " : "";
+
+    txt += "<ul" + mobileClassTxt +">";
     totalLength = tracks.length;
     for (var i = 0; i < tracks.length ; i++) {
-        txt += "<li id='track-" + i +"' data-index=" + i + " data-stream=" + tracks[i].children[2].textContent + ">";
-        txt += "<h3>" + tracks[i].children[0].textContent + "</h3>";
-        txt += "<h2>" + tracks[i].children[1].textContent + "</h2>";
+        txt += "<li id='track-" + i +"' data-index=" + i + " data-stream=" + tracks[i].children[2].textContent + mobileClassTxt + ">";
+        txt += "<h3" + mobileClassTxt + ">" + tracks[i].children[0].textContent + "</h3>";
+        txt += "<h2" + mobileClassTxt + ">" + tracks[i].children[1].textContent + "</h2>";
         txt += "</li>";
     }
     txt += "</div>";
     document.getElementById("list").innerHTML = txt;
+
+    if (isMobile)
+        $("#playbar").addClass("mobile");
 
     $("li").on("click",function() {
 
@@ -119,7 +127,7 @@ function processData(data) {
         lastliclicked = $(this).attr('data-index');
 
         stream.on('end', function(){
-            console.log("finished");
+            // console.log("finished");
             $("#play").addClass("paused");
             var nextSong = 0;
             if (shouldShuffle) {
@@ -167,7 +175,6 @@ function processData(data) {
                 nextSong = 0;
             }
         }
-        console.log(nextSong);
         $("#track-" + nextSong).trigger("click");
     });
 
@@ -183,7 +190,6 @@ function processData(data) {
                 nextSong = 0;
             }
         }
-        console.log(nextSong);
         $("#track-" + nextSong).trigger("click");
     });
 
@@ -244,23 +250,25 @@ function processData(data) {
 $(function() {
     var url = 'http://vip.aersia.net/roster.xml';
 
-    $.ajaxPrefilter( function (options) {
-        if (options.crossDomain && jQuery.support.cors) {
-            var http = (window.location.protocol === 'http:' ? 'http:' : 'https:');
-            options.url = http + '//cors-anywhere.herokuapp.com/' + options.url;
-            //options.url = "http://cors.corsproxy.io/url=" + options.url;
-        }
+    // $.ajaxPrefilter( function (options) {
+    //     if (options.crossDomain && jQuery.support.cors) {
+    //         var http = (window.location.protocol === 'http:' ? 'http:' : 'https:');
+    //         options.url = http + '//cors-anywhere.herokuapp.com/' + options.url;
+    //         //options.url = "http://cors.corsproxy.io/url=" + options.url;
+    //     }
+    // });
+    //
+    // $.get(
+    //     url,
+    //     function (response) {
+    //         console.log("> ", response);
+    //         processData(response)
+    //     });
+
+    xhr('get', 'http://vip.aersia.net/roster.xml?' + Math.round(Math.random() * 10000) , function(data) {
+        // console.log(data);
+        processData(data);
     });
 
-    $.get(
-        url,
-        function (response) {
-            console.log("> ", response);
-            processData(response)
-        });
-
-// xhr('get', 'http://vip.aersia.net/roster.xml?' + Math.round(Math.random() * 10000) , function(data) {
-//
-// });
 });
 
