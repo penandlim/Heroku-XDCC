7eb3b79387f2bde8b70b5a7211ab605be00df9b8
John Lim (Win Desktop) <limjohn@uw.edu>
2017-09-02 23:49:24 -0700

    vip

--- index.js
+++ index.js
@@ -13,6 +13,10 @@ app.get('/', function(request, response) {
   response.render('pages/index');
 });
 
+app.get('/vip/', function(request, response) {
+    response.render('vip/index');
+});
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
