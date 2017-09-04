798fa0a336eb35f13979a6eb49086232e580abc0
John Lim (Win Desktop) <limjohn@uw.edu>
2017-09-03 23:40:07 -0700

    working version

--- xdcctest.js
+++ xdcctest.js
@@ -1,8 +1,6 @@
-var irc = require('irc'),
-    sxdcc = require('sxdcc'),
-    fs = require('fs'),
-    ProgressBar = require('progress');
-
+var irc = require('xdcc').irc;
+var ProgressBar = require('progress');
+var progress;
 
 
 var connectIRC = function (bot, pack) {
@@ -16,50 +14,42 @@ var connectIRC = function (bot, pack) {
         channels: ['#nibl'],
         userName: user,
         realName: user
-    })
-        .on('join', function (channel, nick, message) {
+    });
 
-            if (nick !== user) return;
-            console.log('Joined', channel);
+    client.on('join', function(channel, nick, message) {
+        if (nick !== user) return;
+        console.log('Joined', channel);
+        client.getXdcc(bot, 'xdcc send #' + pack, '.');
+    });
 
-            var progress,
-                last = 0;
-
-            sxdcc(client, bot, pack, start, function (err, conn) {
-                if (err) {
-                    console.log(err);
-                    return;
-                }
-                conn.on('connect', function (meta) {
-                    console.log('Connected: ' + meta.ip + ':' + meta.port);
-                    progress = new ProgressBar('Downloading... [:bar] :percent, :etas remaining', {
-                        incomplete: ' ',
-                        total: meta.filesize,
-                        width: 20
-                    });
-                    this.pipe(fs.createWriteStream(meta.filename));
-                })
-
-                    .on('progress', function (received) {
-                        progress.tick(received - last);
-                        last = received;
-                    })
-
-                    .on('error', function (err) {
-                        console.log('XDCC ERROR: ' + JSON.stringify(err));
-                    });
-            });
-        })
-
-        .on('error', function (err) {
-            console.log("IRC ERROR: " + JSON.stringify(err));
-        })
-
-        .on('notice', function (from, to, message) {
-            if (to == user && from == bot) {
-                console.log("[notice]", message);
-            }
+    client.on('xdcc-connect', function(meta) {
+        console.log('Connected: ' + meta.ip + ':' + meta.port);
+        progress = new ProgressBar('Downloading... [:bar] :percent, :etas remaining', {
+            incomplete: ' ',
+            total: meta.length,
+            width: 20
         });
+    });
+
+    var last = 0;
+    client.on('xdcc-data', function(received) {
+        progress.tick(received - last);
+        last = received;
+    });
+
+    client.on('xdcc-end', function(received) {
+        console.log('Download completed');
+    });
+
+    client.on('notice', function(from, to, message) {
+        if (to == user && from == bot) {
+            console.log("[notice]", message);
+        }
+    });
+
+    client.on('error', function(message) {
+        console.error(message);
+    });
 };
 
 module.exports.connectIRC = connectIRC;
